8bbeee260b053da83005a669c9f3fd6529e77d45
Artur Iskakov <turikm@yandex.ru>
2025-05-13 19:43:53 +0300

    get_question fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+from http import HTTPStatus
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.funcs import get_db
@@ -17,4 +18,9 @@ def get_question(db: Session = Depends(get_db)):
 @app.get("/api/v1/questions/{question_id}", response_model=QuestionRead)
 def get_question(question_id: int, db: Session = Depends(get_db)):
     """Возвращает вопрос."""
-    return db.query(Question).get(question_id)
+    question = db.query(Question).get(question_id)
+    if question is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="question not found")
+    return question
