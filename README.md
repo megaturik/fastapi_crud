3e7410c42c459590541f1eaf67b8d949817aed09
Artur Iskakov <turikm@yandex.ru>
2025-05-15 15:02:17 +0300

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # fastapi_crud
+
+REST API на FastAPI для работы с вопросами и ответами (например, для викторин или обучающих платформ).
+
+В учебных целях, посмотреть FastAPI, Pydantic, SQLAlchemy + Alembic
+
+## Стек
+- SQLAlchemy 2.0.40 + Alembic
+- Pydantic 
+- FastAPI 0.115
+- Python 3.9+
+- Postgres
+
+## Быстрый старт
+
+### Установка
+
+```bash
+git clone https://github.com/megaturik/fastapi_crud
+cd fastapi_crud
+python -m venv venv
+source venv/bin/activate  # или venv\Scripts\activate на Windows
+pip install -r requirements.txt
+```
+### Настройка .env
+```
+POSTGRES_USER = user
+POSTGRES_PASSWORD = password
+POSTGRES_DB = test_db
+POSTGRES_HOST = localhost
+```
+
+## Заполнение базы тестовыми данными:
+В test-data/questions.csv есть некоторе количество подготовленных вопросов.
+
+Можно добавить их в базу:
+
+```bash
+python3 fill_db_from_csv.py
+```
+
+### Запуск приложения
+
+```bash
+fastapi dev app/main.py
+```
+
+Приложение будет доступно по адресу: [http://127.0.0.1:8000](http://127.0.0.1:8000)
+
+## Документация
+
+- Swagger UI: [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
+- ReDoc: [http://127.0.0.1:8000/redoc](http://127.0.0.1:8000/redoc)
+
+##  Эндпоинты
+
+### `GET /api/v1/questions/`
+
+Получить список всех вопросов.
+
+### `POST /api/v1/questions/`
+
+Создать новый вопрос.
+
+#### Пример запроса
+
+```json
+{
+  "text": "Столица Франции?",
+  "difficult_id": 1,
+  "answers": [
+    {"text": "Париж", "is_correct": true},
+    {"text": "Лондон", "is_correct": false}
+  ]
+}
+```
+
+### `GET /api/v1/questions/{question_id}`
+
+Получить вопрос по ID с его ответами.
+
+##  Структура проекта
+
+```
+.
+├── app/
+│   ├── main.py
+│   ├── models.py
+│   ├── schemas.py
+│   └── ...
+├── tests/
+├── openapi.json
+├── requirements.txt
+└── README.md
+```
--- fill_db_from_csv.py
+++ fill_db_from_csv.py
@@ -8,11 +8,6 @@ from app.models import Answer, Difficult_Level, Question
 from app.schemas import DifficultLevelCreate, QuestionCreate
 from app.settings import settings
 
-diff_levels = ('Легкий', 'Средний', 'Сложный', 'Финал')
-
-engine = create_engine(settings.get_database_url(), echo=True)
-session = Session(engine)
-
 
 def create_diff_level(dto: DifficultLevelCreate, db: Session):
     level = Difficult_Level(**dto.model_dump())
@@ -40,30 +35,42 @@ def create_question(dto: QuestionCreate, db: Session):
         raise RuntimeError(f"Ошибка при сохранении вопроса: {str(e)}")
 
 
-for level in diff_levels:
-    create_diff_level(DifficultLevelCreate(name=level), session)
+def main():
+
+    DIFF_LEVELS = ('Легкий', 'Средний', 'Сложный', 'Финал')
+    QUESTION_FILE = 'test-data/questions.csv'
+
+    engine = create_engine(settings.get_database_url())
+    session = Session(engine)
+
+    # Создаемм уровни сложности.
+    for level in DIFF_LEVELS:
+        create_diff_level(DifficultLevelCreate(name=level), session)
+    # Создаем вопросы из csv.
+    with open(QUESTION_FILE) as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for (
+            diff_level,
+            question,
+            answer_a,
+            answer_b,
+            answer_c,
+            answer_d,
+            correct_answer
+        ) in reader:
+            correct_answer = int(correct_answer)
+            data = {
+                "text": question,
+                "difficult_id": diff_level,
+                "answers": [
+                    {"text": answer_a, "is_correct": 1 == correct_answer},
+                    {"text": answer_b, "is_correct": 2 == correct_answer},
+                    {"text": answer_c, "is_correct": 3 == correct_answer},
+                    {"text": answer_d, "is_correct": 4 == correct_answer}
+                ]
+            }
+            question = create_question(QuestionCreate(**data), db=session)
 
 
-with open('questions.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    for (
-        diff_level,
-        question,
-        answer_a,
-        answer_b,
-        answer_c,
-        answer_d,
-        correct_answer
-    ) in reader:
-        correct_answer = int(correct_answer)
-        data = {
-            "text": question,
-            "difficult_id": diff_level,
-            "answers": [
-                {"text": answer_a, "is_correct": 1 == correct_answer},
-                {"text": answer_b, "is_correct": 2 == correct_answer},
-                {"text": answer_c, "is_correct": 3 == correct_answer},
-                {"text": answer_d, "is_correct": 4 == correct_answer}
-            ]
-        }
-        question = create_question(QuestionCreate(**data), db=session)
+if __name__ == '__main__':
+    main()
